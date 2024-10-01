347bd5eedd2e212efb82ec5a8bea336e4e6906af
Mindaugas Rubuzevicius <mindaugas.rubuzevicius@juvare.com>
2024-10-02 00:06:00 +0300

    Added migration files and db initialize.

---
+++ Pharmacies/Pharmacies.API/Extensions/StartupDbExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pharmacies.API.Database;
+
+namespace Pharmacies.API.Extensions;
+
+public static class StartupDbExtensions
+{
+	public static void CreateDatabaseIfNotExists(this IHost host)
+	{
+		using var scope = host.Services.CreateScope();
+		var services = scope.ServiceProvider;
+
+		var pharmaciesDbContext = services.GetRequiredService<PharmaciesDbContext>();
+
+		if (pharmaciesDbContext.Database.EnsureCreated())
+		{
+			pharmaciesDbContext.Database.MigrateAsync();
+		}
+	}
+}
